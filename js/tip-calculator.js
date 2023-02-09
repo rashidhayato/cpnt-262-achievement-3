eedae5d273c24ad9594deaf64e34c942f1889026
rashid <abdulrashid.hayato@edu.sait.ca>
2023-02-08 20:28:33 -0700

    first commit

---
+++ js/tip-calculator.js
@@ -0,0 +1,23 @@
+// Assign initial values
+const billTotal = 40;
+const tipRate = .20;
+
+const tipPercentage = tipRate * 100;
+console.log (tipRate)
+
+
+
+// Calculate tip
+const tipAmount = billTotal * tipRate;
+const paymentTotal = billTotal + tipAmount;
+
+
+// Output
+console.log(tipAmount);
+console.log(tipPercentage);
+console.log(paymentTotal);
+
+console.log('${billTotal}, ${tipRate},+ ${tipPercentage}, {paymentTotal}')
+
+console.log(result);
+Math.round()
